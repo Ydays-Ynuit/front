1bbfbd0179e80201b8b7eedd166a0a2a37b87756
MoustaphaCamara <cmr.mous@gmail.com>
2024-02-06 16:31:54 +0100

    chores(packages): add react-icons

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
 import Logo from "../components/Logo.tsx";
 import ChatMessage from "../components/Chat/ChatMessage.tsx";
 import ButtonYn from "../components/ButtonYN.tsx";
+import {IoSettingsOutline} from "@react-icons/all-files/io5/IoSettingsOutline";
+import {FaUserPlus} from "@react-icons/all-files/fa/FaUserPlus";
+import {BiMessageSquareEdit} from "@react-icons/all-files/bi/BiMessageSquareEdit";
 
 const HomePage = () => {
     return (
@@ -8,6 +11,9 @@ const HomePage = () => {
             <Logo />
             <ChatMessage />
             <ButtonYn content={"Valider"} />
+            <IoSettingsOutline />
+            <FaUserPlus />
+            <BiMessageSquareEdit />
         </div>
     );
 };
