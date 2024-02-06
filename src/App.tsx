ed77200254c29108c566daab4f7c002e5f079394
johanpoyet <johan.poyet@clsi.fr>
2024-02-06 11:36:57 +0100

    create first route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.scss'
+import Logo from './components/Logo.tsx';
 import HomePage from "./pages/HomePage.tsx";
+import { BrowserRouter, Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-        <HomePage />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/logo" element={<Logo />} />
+            <Route path="/home" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
