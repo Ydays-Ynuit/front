302206b154900e25996cc036a7ff5c60152f9672
MoustaphaCamara <cmr.mous@gmail.com>
2024-02-27 16:02:14 +0100

    feat(login): login & registration pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,29 +1,40 @@
 import Logo from "../components/Logo.tsx";
+import ButtonYn from "../components/ButtonYN.tsx";
+import {IoArrowBackCircleOutline} from "@react-icons/all-files/io5/IoArrowBackCircleOutline";
+import {NavLink} from "react-router-dom";
 
 const LoginPage = () => {
     return (
         <>
             <div className="fixed top-0 left-0 w-full">
                 <Logo/>
-                {/*<div className="fixed -top-10 left-0 w-full ml-16 relative text-2xl">*/}
-                {/*    <IoArrowBackCircleOutline/>*/}
-                {/*</div>*/}
+                <NavLink
+                    to="/"
+                    className="text-white "
+                >
+                    <div className="fixed -top-10 left-0 ml-16 relative text-2xl">
+                        <IoArrowBackCircleOutline/>
+                    </div>
+                </NavLink>
             </div>
 
-            <div className="container">
-                <div className="heading">
-                    <h2>Inscription</h2>
+            <div className="container -translate-y-12">
+                <div className="-translate-y-12">
+                    <h2 className="text-xl">Connexion</h2>
                 </div>
                 <div className="content">
-                    <div className="form">
-                        <p>Pseudo</p>
-                        <input type="text" className="bg-black bg-opacity-40 rounded-xl py-1 px-3"
+                    <div className="text-left">
+                        <p className="my-2">Pseudo</p>
+                        <input type="text" className="w-full bg-black bg-opacity-40 rounded-xl py-1 px-3"
                                placeholder="pseudo"/>
                     </div>
-                    <div className="form">
-                        <p>Mot de passe</p>
-                        <input type="text" className="bg-black bg-opacity-40 rounded-xl py-1 px-3"
+                    <div className="text-left mt-4">
+                        <p className="my-2">Mot de passe</p>
+                        <input type="password" className="w-full bg-black bg-opacity-40 rounded-xl py-1 px-3"
                                placeholder="******"/>
+                    </div>
+                    <div className="mt-16">
+                        <ButtonYn content="Connexion"/>
                     </div>
                 </div>
             </div>
--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,8 +1,49 @@
+import Logo from "../components/Logo.tsx";
+import {NavLink} from "react-router-dom";
+import {IoArrowBackCircleOutline} from "@react-icons/all-files/io5/IoArrowBackCircleOutline";
+import ButtonYn from "../components/ButtonYN.tsx";
+
 const RegistrationPage = () => {
     return (
-        <div>
-            RegistrationPage
-        </div>
+        <>
+            <div className="fixed top-0 left-0 w-full">
+                <Logo/>
+                <NavLink
+                    to="/"
+                    className="text-white "
+                >
+                    <div className="fixed -top-10 left-0 ml-16 relative text-2xl">
+                        <IoArrowBackCircleOutline/>
+                    </div>
+                </NavLink>
+            </div>
+
+            <div className="container -translate-y-12">
+                <div className="-translate-y-12">
+                    <h2 className="text-xl">Inscription</h2>
+                </div>
+                <div className="content">
+                    <div className="text-left">
+                        <p className="my-2">Pseudo</p>
+                        <input type="text" className="w-full bg-black bg-opacity-40 rounded-xl py-1 px-3"
+                               placeholder="pseudo"/>
+                    </div>
+                    <div className="text-left mt-4">
+                        <p className="my-2">Mot de passe</p>
+                        <input type="password" className="w-full bg-black bg-opacity-40 rounded-xl py-1 px-3"
+                               placeholder="******"/>
+                    </div>
+                    <div className="text-left">
+                        <p className="my-2">Retaper le mot de passe</p>
+                        <input type="password" className="w-full bg-black bg-opacity-40 rounded-xl py-1 px-3"
+                               placeholder="******"/>
+                    </div>
+                    <div className="mt-16">
+                        <ButtonYn content="Créer profil"/>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
