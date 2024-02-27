b71966021366a1004d2e9f72fcadcc504e67368d
MoustaphaCamara <cmr.mous@gmail.com>
2024-02-27 10:21:15 +0100

    chores(packages): fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js, jsx, ts, tsx}',
-    './components/**/*.{html,js, jsx, ts, tsx}'
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
 }
