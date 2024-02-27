18e1cc1a65f02cdf47d4a2cbc9e460a87c080e76
MoustaphaCamara <cmr.mous@gmail.com>
2024-02-27 14:19:54 +0100

    feat(login): create pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './fonts/Cagliostro.ttf'
 import HomePage from "./pages/HomePage.tsx";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Logo from "./components/Logo.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import RegistrationPage from "./pages/RegistrationPage.tsx";
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/logo" element={<Logo />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/registration" element={<RegistrationPage />} />
           </Routes>
         </BrowserRouter>
   )
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header.tsx";
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+};
+
+export default LoginPage;
