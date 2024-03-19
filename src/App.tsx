61ed12836b5dae08bb5df39aa8fd2a834fa573c6
MoustaphaCamara <cmr.mous@gmail.com>
2024-03-19 11:04:50 +0100

    feat(homepage): homepage with link to login/register

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import './App.scss'
 import './fonts/Cagliostro.ttf'
 import HomePage from "./pages/HomePage.tsx";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Logo from "./components/Logo.tsx";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import LoginPage from "./pages/LoginPage.tsx";
 import RegistrationPage from "./pages/RegistrationPage.tsx";
 
 function App() {
 
-  return (
+    return (
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/logo" element={<Logo />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/registration" element={<RegistrationPage />} />
-          </Routes>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/registration" element={<RegistrationPage/>}/>
+            </Routes>
         </BrowserRouter>
-  )
+    )
 }
 
 export default App
